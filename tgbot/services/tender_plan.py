962217b23f53257f590b89ba1ebeb0b357a77a4b
Zikil <il.zahvatkin33@gmail.com>
2024-05-14 00:32:08 +0700

    update tender plan and sched tenderpro

--- tgbot/services/tender_plan.py
+++ tgbot/services/tender_plan.py
@@ -6,52 +6,53 @@ from thefuzz import fuzz
 from bs4 import BeautifulSoup
 from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 from datetime import datetime
+import time
 
 cookies = {
-    'jwt': 's%3ABearer%20f7dcac67acdb2a348f5c81fc26cfafaba892a7bc02dceb97ffe079ad60b0edb4399522590c067b24459b785fa019006943700cc47033ca26b7aecd22f3777077.UH%2BcCzOylTzLr%2BF6Hf4kerem6GuMoVK%2FBSiOYmPCkEc',
-    'source': 'key=0&tender=6628ae9952e24fc13583dd05',
-    'previousUrl': 'tenderplan.ru%2Fbitrix24%2Finstructions%2F',
-    'tildasid': '1713941269630.966260',
+    'jwt': 's%3ABearer%20a4dc57cc44d5ca62a06a7b19660840a66f3048028b417bbc813a1acf6f3691da841b9120373431377409359f64430b0644ee22ddf072fbe6ad656b57eeebe83d.fv8XBqznQBCV2IGatFCIpsqc3upsd40a7AEZa0kaNTg',
+    'referer': 'https://tenderplan.ru/app?key=0&tender=6639e01152e24fc13574139f',
+    'source': 'key=0&tender=6639e01152e24fc13574139f',
     'tildauid': '1713888711831.359844',
-    'referer': 'https://tenderplan.ru/app',
     '__ddg1_': 'ZKa7JlUseYuy3cvawO9W',
 }
 
 headers = {
     'Accept': '*/*',
-    'Authorization': 'Bearer f7dcac67acdb2a348f5c81fc26cfafaba892a7bc02dceb97ffe079ad60b0edb4399522590c067b24459b785fa019006943700cc47033ca26b7aecd22f3777077',
+    'Authorization': 'Bearer a4dc57cc44d5ca62a06a7b19660840a66f3048028b417bbc813a1acf6f3691da841b9120373431377409359f64430b0644ee22ddf072fbe6ad656b57eeebe83d',
     'Sec-Fetch-Site': 'same-origin',
     'Accept-Language': 'ru',
     # 'Accept-Encoding': 'gzip, deflate, br',
     'Sec-Fetch-Mode': 'cors',
     'Host': 'tenderplan.ru',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15',
-    'Referer': 'https://tenderplan.ru/app?key=0&tender=6628ae9952e24fc13583dd05',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15',
     'Connection': 'keep-alive',
-    # 'Cookie': 'jwt=s%3ABearer%20f7dcac67acdb2a348f5c81fc26cfafaba892a7bc02dceb97ffe079ad60b0edb4399522590c067b24459b785fa019006943700cc47033ca26b7aecd22f3777077.UH%2BcCzOylTzLr%2BF6Hf4kerem6GuMoVK%2FBSiOYmPCkEc; source=key=0&tender=6628ae9952e24fc13583dd05; previousUrl=tenderplan.ru%2Fbitrix24%2Finstructions%2F; tildasid=1713941269630.966260; tildauid=1713888711831.359844; referer=https://tenderplan.ru/app; __ddg1_=ZKa7JlUseYuy3cvawO9W',
+    'Referer': 'https://tenderplan.ru/app?key=0&tender=6639e01152e24fc13574139f',
+    # 'Cookie': 'jwt=s%3ABearer%20a4dc57cc44d5ca62a06a7b19660840a66f3048028b417bbc813a1acf6f3691da841b9120373431377409359f64430b0644ee22ddf072fbe6ad656b57eeebe83d.fv8XBqznQBCV2IGatFCIpsqc3upsd40a7AEZa0kaNTg; referer=https://tenderplan.ru/app?key=0&tender=6639e01152e24fc13574139f; source=key=0&tender=6639e01152e24fc13574139f; tildauid=1713888711831.359844; __ddg1_=ZKa7JlUseYuy3cvawO9W',
     'Sec-Fetch-Dest': 'empty',
-    'Socket': 'ZTvzcuitnjsM-m4OBCP4',
 }
 
-def search_in_autopiter(search: str):
-    cookies = {
-        'guestId': '0_3IPr7jh-WiY0mUjEdAk',
-        'app_state': '{%22cart%22:{%22enteredComment%22:%22%22%2C%22sort%22:%22none%22%2C%22selectedDetailUids%22:[]%2C%22selectedDeliveryType%22:%221%22%2C%22cartViewMode%22:0}%2C%22appraiseProduct%22:{%22appraiseCardOpenState%22:0}%2C%22ordersInfo%22:{%22ordersInfo%22:{%22disabledColumns%22:null}}%2C%22user%22:{%22confirmCity%22:false%2C%22isWholesaleWidgetOpen%22:false%2C%22mobileMenuListToggle%22:{}%2C%22newSiteLoyalty%22:false}%2C%22mobileVersion%22:{%22isMobileDevice%22:false}%2C%22abTest%22:[]%2C%22server%22:{%22domainDebug%22:%22%22}}',
-    }
 
-    headers = {
-        'Accept': 'application/json, text/plain, */*',
-        'Sec-Fetch-Site': 'same-origin',
-        # 'Cookie': 'guestId=0_3IPr7jh-WiY0mUjEdAk; app_state={%22cart%22:{%22enteredComment%22:%22%22%2C%22sort%22:%22none%22%2C%22selectedDetailUids%22:[]%2C%22selectedDeliveryType%22:%221%22%2C%22cartViewMode%22:0}%2C%22appraiseProduct%22:{%22appraiseCardOpenState%22:0}%2C%22ordersInfo%22:{%22ordersInfo%22:{%22disabledColumns%22:null}}%2C%22user%22:{%22confirmCity%22:false%2C%22isWholesaleWidgetOpen%22:false%2C%22mobileMenuListToggle%22:{}%2C%22newSiteLoyalty%22:false}%2C%22mobileVersion%22:{%22isMobileDevice%22:false}%2C%22abTest%22:[]%2C%22server%22:{%22domainDebug%22:%22%22}}',
-        'Sec-Fetch-Dest': 'empty',
-        'Accept-Language': 'ru',
-        'Sec-Fetch-Mode': 'cors',
-        'Host': 'autopiter.ru',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15',
-        'Referer': 'https://autopiter.ru/goods/%D1%84%D0%BE%D1%80%D1%81%D1%83%D0%BD%D0%BA%D0%B0%20%D1%82%D0%BE%D0%BF%D0%BB%D0%B8%D0%B2%D0%BD%D0%B0%D1%8F%20common%20rail%20%D0%BA%D0%B0%D0%BC%D0%B0%D0%B7',
-        # 'Accept-Encoding': 'gzip, deflate, br',
-        'Connection': 'keep-alive',
-    }
+def split_search(search_string):
+    search_split = []
+    digit_count = sum(char.isdigit() for char in search_string)
+    if digit_count <= 1:
+        return []
+    for word in search_string.split(" "):
+        digit_count = sum(char.isdigit() for char in word)
+        if (
+            digit_count >= len(word) // 4
+            and len(word)>=6 
+            and not(bool(re.search('[а-яА-Я]', word)))
+
+        ):
+            search_split.append(word)
+    # if bool(re.search(r'\d', good_name)):
+    #     continue
+    return search_split
+
+
+def search_in_autopiter(search: str):
+    
 
     params = {
         'detailNumber': search,
@@ -60,14 +61,14 @@ def search_in_autopiter(search: str):
 
     try:
         ap_search = search
-        resp = requests.get('https://autopiter.ru/api/api/searchdetails', params=params, cookies=cookies, headers=headers)
+        resp = requests.get('https://autopiter.ru/api/api/searchdetails', params=params)
         print("response - ", resp.status_code)
         print("good search - ", ap_search)
         goodauto = []
 
         if resp.status_code == 200:
             params = {'idArticles': [position.get('id') for position in resp.json().get('data').get('catalogs')]}
-            get_cost_resp = requests.get('https://autopiter.ru/api/api/appraise/getcosts', params=params, cookies=cookies, headers=headers)
+            get_cost_resp = requests.get('https://autopiter.ru/api/api/appraise/getcosts', params=params)
             print(get_cost_resp)
             
             for position in resp.json().get('data').get('catalogs'):
@@ -90,11 +91,12 @@ def search_in_autopiter(search: str):
                     'ap_number': ap_number,
                     'ap_originalPrice': ap_originalPrice,
                     'link_autopiter': f"https://autopiter.ru/goods/{ap_number}/{position.get('catalogUrl')}/id{ap_id}",
+                    'ap_id': ap_id,
                     })
         elif resp.status_code == 429:
             raise Exception("429")
         if goodauto != []:
-            goodauto = pd.DataFrame(goodauto).sort_values(by="fuzz",ascending=False)[:2]
+            goodauto = pd.DataFrame(goodauto).sort_values(by="fuzz",ascending=False)[:1]
         elif goodauto == []: 
             goodauto = [{
                     'ap_search': '----',
@@ -117,14 +119,12 @@ def search_in_autopiter(search: str):
         return []
 
 
-def tenders_with_goods(pagecount = 1):
+def tenders_with_goods(pagecount: int = 1):
     try:
-
         tenders_with_goods = []
-
         count = 0
         page = 0
-
+        countn = 50
         while True:
             try:
                 params = {'page': page}
@@ -133,9 +133,11 @@ def tenders_with_goods(pagecount = 1):
                 #     break
                 if (page>=pagecount):
                     break
-                
+                # if (count>countn):
+                #     break
                 page += 1
                 tenders = response.json().get('tenders')
+                print(f"--------{len(tenders)}")
                 for tend in tenders:
                     try:
                         tend_name = tend.get('orderName')
@@ -147,48 +149,84 @@ def tenders_with_goods(pagecount = 1):
                         response = requests.get('https://tenderplan.ru/api/tenders/get', params=params, cookies=cookies, headers=headers)
                         if "ObjectInfo" in response.json().get('json'):
                             goods = json.loads(response.json().get('json'))["0"]["fv"]["0"]["fv"]["tb"]
-                            ###
                             submission_close_timestamp = int(json.loads(response.json().get('json'))['1']['fv']['1']['fv'])
                             print(submission_close_timestamp)
-                            # submission_close_datetime = datetime.fromtimestamp(submission_close_timestamp)#.fromtimestamp(submission_close_timestamp / 1000)
-                            # submission_close_datetime = datetime.datetime.strftime(submission_close_timestamp, '%d/%m/%Y')
                             submission_close_datetime = datetime.fromtimestamp(submission_close_timestamp/1000).strftime('%Y-%m-%d %H:%M:%S')
                             for good in goods:
-                                count += 1
-                                # if (count>50):
+                                # if (count>countn):
                                 #     break
                                 good_name = goods.get(good).get('0').get('fv')
-                                # print(f"name - {good_name}")
-                                ap_search = search_in_autopiter(good_name)
-                                for ap_s in ap_search.iterrows():
-                                    # print(ap_s)
-                                    tenders_with_goods.append({
-                                        "tend_name": tend_name,
-                                        "tend_link": f"https://tenderplan.ru/app?tender={id}",
-                                        "tend_under": submission_close_datetime,
-                                        "good_name": good_name,
-                                        "ap_search_fuzz": ap_s[1].get('ap_name'),
-                                        "ap_search_name": ap_s[1].get('fuzz'),
-                                        "ap_search_link": ap_s[1].get('link_autopiter'),
-                                        "ap_search_price": ap_s[1].get('ap_originalPrice'),
-                                    })
+                                print(f"name - {good_name}")
+                                # count += 1
+                                
+                                for name in split_search(good_name):
+                                    ap_search = search_in_autopiter(name)
+                                    for ap_s in ap_search.iterrows():
+                                        # print(ap_s)
+                                        count += 1
+                                        print("count",count)
+                                        tenders_with_goods.append({
+                                            "tend_name": tend_name,
+                                            "tend_link": f"https://tenderplan.ru/app?tender={id}",
+                                            "tend_under": submission_close_datetime,
+                                            "good_name": good_name,
+                                            "ap_good_name": name,
+                                            "ap_search_name": ap_s[1].get('ap_name'),
+                                            "ap_search_fuzz": ap_s[1].get('fuzz'),
+                                            "ap_search_link": ap_s[1].get('link_autopiter'),
+                                            "ap_id": ap_s[1].get('ap_id'),
+                                            "ap_search_price": ap_s[1].get('ap_originalPrice'),
+                                        })
                             # break
                     except Exception as e:
                         print("error -- ",e)
-
             except Exception as e:
-                print("errror - ",e)
+                print(e)
         print(f"count - {count}")
 
-        for twg in tenders_with_goods:
-            print(twg)
+        # for twg in tenders_with_goods:
+        #     print(twg)
 
+        print(f"excel!!!!!!!!!")
+        tenders_with_goods = get_all_price(tenders_with_goods)
         tends = pd.DataFrame(tenders_with_goods)
         tends.to_excel(r'tgbot/data/tenders_with_goods.xlsx')
+        print(f"excel!!!!!!!!!2")
+
 
         return tenders_with_goods
     except Exception as e:
         print(e)
+
+
+def get_all_price(tenders_with_goods):
+    ap_ids = []
+    c = 0
+    tenders_with_goods2 = []
+    for ap in tenders_with_goods:
+        ap_ids.append(ap.get('ap_id'))
+    
+    params = {'idArticles': ap_ids}
+    get_cost_resp = requests.get('https://autopiter.ru/api/api/appraise/getcosts', params=params)
+    while get_cost_resp.status_code == 429:
+        time.sleep(5)
+        c += 1
+        get_cost_resp = requests.get('https://autopiter.ru/api/api/appraise/getcosts', params=params)
+        if c>10: break
+
+    print(get_cost_resp.status_code)
+    cost_resp = get_cost_resp.json().get('data')
+    for ap in tenders_with_goods:
+        try:
+            id = ap.get("ap_id")
+            cost = [cost.get('originalPrice') for cost in cost_resp if cost.get('id') == id]
+            
+            if cost:
+                ap['ap_search_price'] = cost[0]
+            tenders_with_goods2.append(ap)
+        except Exception as e:
+            print(e)
+    return tenders_with_goods2
 
 
 def beautitext(text: str):
--- tgbot/utils/misc_functions.py
+++ tgbot/utils/misc_functions.py
@@ -1,14 +1,16 @@
 # - *- coding: utf- 8 - *-
 from aiogram import Bot
-from aiogram.types import FSInputFile
+from aiogram.types import FSInputFile, BufferedInputFile
 
 from tgbot.data.config import get_admins, PATH_DATABASE, start_status
 from tgbot.utils.const_functions import get_date, send_admins
 from tgbot.utils.misc.bot_logging import bot_logger
-from tgbot.services.parser_tendors import get_tenders_from_url
+from tgbot.services.parser_tendors import get_tenders_from_url, get_excel_from_tenders
 from tgbot.utils.misc.bot_filters import get_employees
 from tgbot.services.tender_plan import tenders_with_goods
-
+import os
+import io
+import pandas as pd
 
 # Отправка сообщения пользователям по тендорам
 # async def get_employees(bot: Bot, text: str, markup=None, not_me=0):
@@ -27,7 +29,7 @@ async def send_employees(bot: Bot, text: str, markup=None, not_me=0):
                         # disable_web_page_preview=True,
                         disable_notification=True,
                     )
-    
+
     # for admin in get_admins():
     #     try:
     #         if str(admin) != str(not_me):
@@ -65,15 +67,29 @@ async def tenders_sched(bot: Bot):
     try:
         tenders_id = await get_tenders_from_url()
         bot_logger.warning(f"tenders_id: {tenders_id}")
-        answ = ""
-        for num, tend in enumerate(tenders_id):
-            answ += f"{num+1}. Наименование/артикул: {tend['article']}, id тендера: {tend['id_tender']}, прием до: {tend['date_until']}, url: {tend['url_tender']} \n \n"
-        mes = f"Автоматический поиск тендоров: \n \n"
-        if answ == "":
-            mes += "Ничего не найдено"
+        tenders_id = await get_tenders_from_url()
+        bot_logger.warning(f"tenders_id: {tenders_id}")
+        if (len(str(tenders_id))>4000):
+            tenders_id = pd.DataFrame(tenders_id)
+            get_excel_from_tenders(tenders_id)
+            with io.BytesIO() as output:
+                tenders_id.to_excel(output) 
+                excel_data = output.getvalue()
+            file_excel = io.BytesIO(excel_data)
+            get_empl = get_employees()
+            bot_logger.warning(f"employee1 {get_empl}")
+            for empl in get_empl:
+                await bot.send_document(empl.user_id,BufferedInputFile(file_excel.getvalue(), f"everyday_articles.xlsx"), caption = f"Нашлось по расписанию")
         else:
-            mes += answ
-            await send_employees(bot, mes)
+            answ = ""
+            for num, tend in enumerate(tenders_id):
+                answ += f"{num+1}. Наименование/артикул: {tend['article']}, id тендера: {tend['id_tender']}, прием до: {tend['date_until']}, url: {tend['url_tender']} \n \n"
+            mes = f"Автоматический поиск тендоров: \n \n"
+            if answ == "":
+                mes += "Ничего не найдено"
+            else:
+                mes += answ
+                await send_employees(bot, mes)
     except:
         send_admins(bot, f"Ошибка при автоматическом поиске")
 
